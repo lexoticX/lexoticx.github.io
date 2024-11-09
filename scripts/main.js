10e3dc4e749d7c49f1a6aafa6eef2437676ec72e
lexotic <lexbeuk@gmail.com>
2024-11-09 17:10:43 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,22 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
     fromElement: true,
     width: 'auto',
     height: '100vh',
-    storageManager: { type: null }, // Disable storage manager for now
-    panels: {
-      defaults: [
-        { id: 'layers', el: '.gjs-layers-container', resizable: { tc: 0, cl: 0, cr: 0, bc: 0 } },
-      ],
-    },
+    storageManager: { type: null },
     blockManager: {
       appendTo: '#blocks',
     }
-  });
-
-  // Example component for a clickable box
-  editor.BlockManager.add('clickable-box', {
-    label: 'Clickable Box',
-    content: `<div class="box" onclick="location.href='subpage.html'">Click Me</div>`,
-    category: 'Basic',
-    attributes: { class: 'fa fa-square' }
   });
 });
