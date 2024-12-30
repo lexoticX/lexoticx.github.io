70e139545e895b2d27eb740687b03b7da7eec3cb
lexotic <lexbeuk@gmail.com>
2024-12-30 12:09:38 +0100

    Update script.js

--- datapack-updater/script.js
+++ datapack-updater/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const loadingSpinner = document.getElementById('loadingSpinner');
     const fileNameDisplay = document.getElementById('fileName');
 
-    fetch('https://api.github.com/repos/lexoticX/lexoticx.github.io/contents/versions')
+    fetch('https://api.github.com/repos/lexoticX/lexoticx.github.io/contents/datapack-updater/versions')
         .then(response => response.json())
         .then(files => {
             const versions = files.filter(file => file.name.endsWith('.txt')).map(file => file.name.replace('.txt', ''));
@@ -54,62 +54,3 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
-
-async function processZip(zipFile, fromVersion, toVersion, includecreditss) {
-    const jszip = new JSZip();
-    const zip = await jszip.loadAsync(zipFile);
-
-    if (!Object.keys(zip.files).some(name => name.endsWith('pack.mcmeta'))) {
-        alert('The zip file must contain a pack.mcmeta file.');
-        return;
-    }
-
-    let currentVersion = parseInt(fromVersion);
-    const endVersion = parseInt(toVersion);
-
-    while (currentVersion < endVersion) {
-        const nextVersion = currentVersion + 1;
-        await updateVersion(zip, currentVersion, nextVersion, includecreditss);
-        currentVersion = nextVersion;
-    }
-
-    const blob = await zip.generateAsync({ type: "blob" });
-    saveAs(blob, zipFile.name);
-}
-
-async function updateVersion(zip, fromVersion, toVersion, includecreditss) {
-    const fromVersionContent = await fetchVersionContent(fromVersion);
-    const toVersionContent = await fetchVersionContent(toVersion);
-    const fromVersionThirdLine = getThirdLine(fromVersionContent);
-    const toVersionThirdLine = getThirdLine(toVersionContent);
-
-    const folders = Object.keys(zip.files).filter(name => zip.files[name].dir && !name.startsWith('data/'));
-    const targetFolder = folders.length > 0 ? folders[0] : '';
-
-    await Promise.all(Object.keys(zip.files).map(async (relativePath) => {
-        const zipEntry = zip.files[relativePath];
-        if (zipEntry.name.endsWith('pack.mcmeta')) {
-            const content = await zipEntry.async("string");
-            const modifiedContent = content.replace(new RegExp(fromVersionThirdLine, 'g'), toVersionThirdLine);
-            zip.file(zipEntry.name, modifiedContent);
-        } else if (includecreditss && zipEntry.name.endsWith('creditss.txt')) {
-            const content = await zipEntry.async("string");
-            const newContent = `creditss for lexoticX\n${content}`;
-            zip.file(zipEntry.name, newContent);
-        }
-    }));
-
-    if (includecreditss && !zip.files[`${targetFolder}creditss.txt`]) {
-        zip.file(`${targetFolder}creditss.txt`, 'creditss for lexoticX\n');
-    }
-}
-
-async function fetchVersionContent(version) {
-    const response = await fetch(`https://raw.githubusercontent.com/lexoticX/lexoticx.github.io/main/versions/${version}.txt`);
-    return response.text();
-}
-
-function getThirdLine(content) {
-    const lines = content.split('\n');
-    return lines[2] || ''; // Return the third line or an empty string if it doesn't exist
-}
